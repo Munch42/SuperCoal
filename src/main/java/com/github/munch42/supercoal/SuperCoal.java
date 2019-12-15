620e59a9857e02753fe516419275945424926857
Munch42 <munch4242@gmail.com>
2019-12-14 17:57:24 -0700

    Added the Super Coal Item and basic item registration code.
    Signed-off-by: Munch42 <Munch4242@gmail.com>

--- src/main/java/com/github/munch42/supercoal/SuperCoal.java
+++ src/main/java/com/github/munch42/supercoal/SuperCoal.java
@@ -1,4 +1,55 @@
 package com.github.munch42.supercoal;
 
+import com.github.munch42.supercoal.lists.ItemList;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SuperCoal.modid)
 public class SuperCoal {
+    public static final String modid = "supercoal";
+    public static SuperCoal instance;
+    private static final Logger logger = LogManager.getLogger(modid);
+
+    public SuperCoal(){
+        instance = this;
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event){
+
+    }
+
+    private void clientRegistries(final FMLClientSetupEvent event){
+
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents{
+        @SubscribeEvent
+        public static void registerItems(final RegistryEvent.Register<Item> event){
+            event.getRegistry().registerAll(
+                ItemList.super_coal = new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName(location("super_coal"))
+            );
+
+            logger.info("Items Registered");
+        }
+
+        private static ResourceLocation location(String name){
+            return new ResourceLocation(modid, name);
+        }
+    }
 }
---
+++ src/main/java/com/github/munch42/supercoal/lists/ItemList.java
@@ -0,0 +1,9 @@
+package com.github.munch42.supercoal.lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList {
+
+    public static Item super_coal;
+
+}
